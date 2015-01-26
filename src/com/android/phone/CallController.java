689a8d2045f38cc9291672d39793e0c966c8564d
Shaun Eaton <shaunsibu@gmail.com>
2015-01-26 15:23:25 -0700

    Revert "[2/2] Telephony: implement noise suppression for phone calls"
    This reverts commit 48bcd01ce123dfe623cf62e868cfd3e789689e3c.
    
    Change-Id: Id98d05ee23db2fd89c4c8fd3c6b041ffa4e15b79

--- src/com/android/phone/CallController.java
+++ src/com/android/phone/CallController.java
@@ -272,9 +272,6 @@ public class CallController extends Handler {
         // in-call UI.  Or if there was an error, the InCallScreen will
         // notice the InCallUiState pending call status code flag and display an
         // error indication instead.)
-
-        // enable noise suppression
-        PhoneUtils.turnOnNoiseSuppression(mApp.getApplicationContext(), true);
     }
 
     /**
--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -182,7 +182,6 @@ public class CallFeaturesSetting extends PreferenceActivity
     private static final String BUTTON_VIDEO_CALL_FB_KEY = "videocall_setting_fb_key";
     private static final String BUTTON_VIDEO_CALL_FW_KEY = "videocall_setting_fw_key";
     private static final String BUTTON_VIDEO_CALL_SP_KEY = "vt_imageplacer";
-    private static final String BUTTON_NOISE_SUPPRESSION_KEY = "button_noise_suppression_key";
 
     private static final String BUTTON_GSM_UMTS_OPTIONS = "button_gsm_more_expand_key";
     private static final String BUTTON_CDMA_OPTIONS = "button_cdma_more_expand_key";
@@ -271,7 +270,6 @@ public class CallFeaturesSetting extends PreferenceActivity
     private SwitchPreference mButtonHAC;
     private ListPreference mButtonDTMF;
     private ListPreference mButtonTTY;
-    private SwitchPreference mButtonNoiseSuppression;
     private Preference mPhoneAccountSettingsPreference;
     private SwitchPreference mMwiNotification;
     private ListPreference mVoicemailProviders;
@@ -575,12 +573,6 @@ public class CallFeaturesSetting extends PreferenceActivity
             return true;
         } else if (preference == mButtonVideoCallPictureSelect) {
             startActivity(getVTCallImageSettingsIntent());
-            return true;
-        } else if (preference == mButtonNoiseSuppression) {
-            int nsp = mButtonNoiseSuppression.isChecked() ? 1 : 0;
-            // Update Noise suppression value in Settings database
-            Settings.System.putInt(mPhone.getContext().getContentResolver(),
-                    Settings.System.NOISE_SUPPRESSION, nsp);
             return true;
         }
         return false;
@@ -1663,7 +1655,6 @@ public class CallFeaturesSetting extends PreferenceActivity
         mButtonAutoRetry = (SwitchPreference) findPreference(BUTTON_RETRY_KEY);
         mButtonHAC = (SwitchPreference) findPreference(BUTTON_HAC_KEY);
         mButtonTTY = (ListPreference) findPreference(BUTTON_TTY_KEY);
-        mButtonNoiseSuppression = (SwitchPreference) findPreference(BUTTON_NOISE_SUPPRESSION_KEY);
         mVoicemailProviders = (ListPreference) findPreference(BUTTON_VOICEMAIL_PROVIDER_KEY);
         mIPPrefixPreference = (PreferenceScreen) findPreference(BUTTON_IPPREFIX_KEY);
 
@@ -1727,15 +1718,6 @@ public class CallFeaturesSetting extends PreferenceActivity
             } else {
                 prefSet.removePreference(mButtonTTY);
                 mButtonTTY = null;
-            }
-        }
-
-        if (mButtonNoiseSuppression != null) {
-            if (getResources().getBoolean(R.bool.has_in_call_noise_suppression)) {
-                mButtonNoiseSuppression.setOnPreferenceChangeListener(this);
-            } else {
-                prefSet.removePreference(mButtonNoiseSuppression);
-                mButtonNoiseSuppression = null;
             }
         }
 
