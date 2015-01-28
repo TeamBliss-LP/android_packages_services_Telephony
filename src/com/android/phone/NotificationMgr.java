6a40b89fe415770f40ded21f15b4bf5e87848589
Shaun Eaton <shaunsibu@gmail.com>
2015-01-28 14:18:59 -0700

    Breathing Voicemail Notification from Bliss Stalk.
    Change-Id: Ia338f45393bb51d690614418938b2fb62fbda2db

--- src/com/android/phone/NotificationMgr.java
+++ src/com/android/phone/NotificationMgr.java
@@ -27,11 +27,14 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.pm.UserInfo;
 import android.net.Uri;
+import android.provider.Settings;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
@@ -292,8 +295,14 @@ public class NotificationMgr {
                 R.drawable.stat_notify_voicemail_sub3};
 
         int notificationId = getNotificationId(VOICEMAIL_NOTIFICATION, phoneId);
+        int resId; 
         if (visible) {
-            int resId = android.R.drawable.stat_notify_voicemail;
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.KEY_VOICEMAIL_BREATH, 0) == 1) {
+                resId = R.drawable.stat_notify_voicemail_breath;
+            } else {
+                resId = android.R.drawable.stat_notify_voicemail;
+            }
             if (PhoneUtils.isMultiSimEnabled()) {
                 resId = mwiIcon[phoneId];
             }
